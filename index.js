e623b0d2d157b3c742ca434d36a2e73f38ddfd10
jeldcf <jeldcf@mst.edu>
2021-06-09 16:58:15 -0500

    pug and bowsersync added

--- index.js
+++ index.js
@@ -18,12 +18,16 @@ const port = process.env.PORT || "8000";
  *  App Configuration
  */
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "public")));
+
 /**
  * Routes Definitions
  */
 
 app.get("/", (req, res) => {
-  res.status(200).send("JacksCoins: Lookup crypto exchange rates.");
+   res.render("index", { title: "Home" });
 });
 
 /**
